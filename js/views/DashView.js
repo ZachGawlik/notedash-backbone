fde3c3c27f10e7dcf726b95e3418e6d242ab7caa
Zach Gawlik <zachgawlik@gmail.com>
2014-07-19 17:03:38 -0400

    Implement deleting notes

--- js/views/DashView.js
+++ js/views/DashView.js
@@ -7,7 +7,7 @@ app.DashView = Backbone.View.extend({
         'keydown #new-note': 'addOnEnter'
     },
 
-    initialize: function(initialNotes) {
+    initialize: function() {
         this.$input = $('#new-note');
         this.collection = new app.NoteCollection();
         this.collection.fetch();
@@ -16,29 +16,28 @@ app.DashView = Backbone.View.extend({
     },
 
     render: function () {
-        this.collection.each(function( note ) {
-            this.renderNote( note );
+        this.collection.each(function(note) {
+            this.renderNote(note);
         }, this);
     },
 
     renderNote: function(note) {
         var noteView = new app.NoteView({model: note});
-        this.$el.append( noteView.render().el );
+        $('#notes-container').prepend( noteView.render().el );
     },
 
     addNote: function() {
         var newText = this.$input.val();
         this.$input.val('').trigger('autosize.resize');
         if (newText) {
-            this.collection.create({id: this.collection.nextId(), text: newText})
+            this.collection.create({id: this.collection.nextId(), text: newText, tags: ['#testtag']})
         }
     },
 
     addOnEnter: function(e) {
-        if ((event.keyCode == 13) && (!event.shiftKey)) {
+        if ((event.keyCode === 13) && (!event.shiftKey)) {
             e.preventDefault();
             this.addNote();
         }
-        return;
     }
 });
--- js/views/NoteView.js
+++ js/views/NoteView.js
@@ -5,8 +5,31 @@ app.NoteView = Backbone.View.extend({
     className: 'note-container row',
     template: _.template( $('#note-template').html() ),
 
+    events: {
+        'click .note-content': 'slide',
+        'click .confirm-delete.delete-button': 'deleteNote'
+    },
+
     render: function () {
         this.$el.html(this.template(this.model.attributes));
         return this;
+    },
+
+    slide: function () {
+        var $note = this.$el.find('.note-content');
+        var $deleteButton = this.$el.find('.delete-button');
+        $deleteButton.height($note.height());
+
+        $note.toggleClass('confirm-delete');
+        $note.toggleClass('col-xs-offset-2');
+        $note.toggleClass('col-xs-offset-1');
+        $deleteButton.toggleClass('confirm-delete');
+        $deleteButton.toggleClass('col-xs-0');
+        $deleteButton.toggleClass('col-xs-1');
+    },
+
+    deleteNote: function () {
+        this.model.destroy();
+        this.remove();
     }
 });
