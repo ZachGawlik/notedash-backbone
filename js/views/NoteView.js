37e97d767f406b438c6b8fb0ca7aed6d953f0f34
Zach Gawlik <zachgawlik@gmail.com>
2014-08-14 22:28:39 -0400

    Re-implement editing via textareas

--- js/views/NoteView.js
+++ js/views/NoteView.js
@@ -8,49 +8,60 @@ app.NoteView = Backbone.View.extend({
     events: {
         'click .note-content': 'turnOnEditMode',
         'mousedown .delete-button': 'deleteNote',
-        'keyup .note-text': 'adjustButton',
-        'keydown .note-text': 'saveOnEnter'
+        'keyup .note-text-edit': 'adjustButton',
+        'keydown .note-text-edit': 'saveOnEnter'
     },
 
     initialize: function() {
+        this.render();
         this.listenTo(this.model, 'checkHidden', this.toggleHidden);
         this.listenTo(this.model, 'closeEdit', this.turnOffEditMode);
+        this.listenTo(this.model, 'change', this.render);
     },
 
     render: function () {
         this.$el.html(this.template(this.model.attributes));
+        this.$el.find('.note-text-edit').autosize({append: ''});
+        console.log('render called');
         return this;
     },
 
-    turnOnEditMode: function () {
-        var $note = this.$el.find('.note-content');
+    showDeleteButton: function () {
+        var $noteContent = this.$el.find('.note-content');
         var $deleteButton = this.$el.find('.delete-button');
-        $deleteButton.height($note.height());
 
+        $noteContent.removeClass('col-xs-offset-2').addClass('col-xs-offset-1');
+        $deleteButton.removeClass('col-xs-0').addClass('col-xs-1');
+        $deleteButton.height($noteContent.height());
+    },
+
+    hideDeleteButton: function () {
+        var $noteContent = this.$el.find('.note-content');
+        var $deleteButton = this.$el.find('.delete-button');
+        $noteContent.removeClass('col-xs-offset-1').addClass('col-xs-offset-2');
+        $deleteButton.removeClass('col-xs-1').addClass('col-xs-0');
+    },
+
+    turnOnEditMode: function () {
+        if (this.$el.hasClass('edit-mode')) return;
+
+        var currentNote = this.model;
         app.Notes.each(function(note) {
-            note.trigger('closeEdit');
+            if (note !== currentNote) note.trigger('closeEdit');
         });
 
         this.$el.addClass('edit-mode');
-        $note.removeClass('col-xs-offset-2');
-        $note.addClass('col-xs-offset-1');
-        $deleteButton.removeClass('col-xs-0');
-        $deleteButton.addClass('col-xs-1');
+        this.showDeleteButton();
     },
 
     turnOffEditMode: function () {
         if (!this.$el.hasClass('edit-mode')) return;
 
-        var $note = this.$el.find('.note-content');
-        var $deleteButton = this.$el.find('.delete-button');
-
         this.$el.removeClass('edit-mode');
-        $note.removeClass('col-xs-offset-1');
-        $note.addClass('col-xs-offset-2');
-        $deleteButton.removeClass('col-xs-1');
-        $deleteButton.addClass('col-xs-0');
+        this.hideDeleteButton();
+        this.$el.find('.note-text-edit').blur();
 
-        var inputText = this.$el.find('.note-text').html();
+        var inputText = this.$el.find('.note-text-edit').val().trim();
         if (inputText) {
             this.model.save({text: inputText});
         } else {
@@ -59,14 +70,15 @@ app.NoteView = Backbone.View.extend({
     },
 
     adjustButton: function () {
-        var $note = this.$el.find('.note-content');
+        var $noteContent = this.$el.find('.note-content');
         var $deleteButton = this.$el.find('.delete-button');
-        $deleteButton.height($note.height());
+        $deleteButton.height($noteContent.height());
     },
 
     saveOnEnter: function () {
         if (event.keyCode === 13 && !event.shiftKey) {
             event.preventDefault();
+            this.model.trigger('closeEdit');
         }
     },
 
